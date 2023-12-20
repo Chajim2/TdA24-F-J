d633b591850164b0bef7e1fe98f3b9c82b89234f
Chajim2 <112813231+Chajim2@users.noreply.github.com>
2023-12-20 09:10:20 +0100

    first test fix

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Hello Tour de App! Edited Twice :), Hello Tda, hope its working"
+    return "Hello TdA"
 
 @app.route('/api')
 def api(methods = ["GET", "POST"]):
